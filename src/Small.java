c9dc659a9862770d4a9b92a6319019c4c37e8367
Mike Knoles <mknoles78@gmail.com>
2021-11-20 17:59:22 -0500

    declared variables

--- src/Small.java
+++ src/Small.java
@@ -1,10 +1,21 @@
+
 //Exercise 5.11 Solution: Small.java
 //Author Mike Knoles
 //Program finds the smallest of several integers
+import java.util.Scanner;
+
 public class Small {
 
+	// finds the smallest integer
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		int smallest = 0; // smallest number
+		int number = 0; // number entered by user
+		int integers; // number of integers
+
+		System.out.print("Enter number of integers: ");
+		integers = input.nextInt();
 
 	}
 
