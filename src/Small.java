e1a2e80ef77ba565ed73d9765e2684e408a733cc
Mike Knoles <mknoles78@gmail.com>
2021-11-20 17:53:28 -0500

    inital commit

---
+++ src/Small.java
@@ -0,0 +1,11 @@
+//Exercise 5.11 Solution: Small.java
+//Author Mike Knoles
+//Program finds the smallest of several integers
+public class Small {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
